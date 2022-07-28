1c42a62b55095284f03b2325a385019c09cb702f
Sylwia Modelewska <sylwiasupera@wp.pl>
2022-07-28 17:55:27 +0200

    Change script deleting arguments declarations

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ function buttonClicked(argButtonName) {
   clearMessages();
   console.log(argButtonName + ' został kliknięty');
 
-  var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput;
+  var computerMove, playerMove, randomNumber;
 
 /**
  * Funkcja przypisująca nazwę wybranego argumentu do wybranej liczby
